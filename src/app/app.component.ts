b104e68ba1c25a171fc2798654222fb09c2e416f
amin <aminazarpey@gmail.com>
2025-04-09 17:20:20 +0330

    feat : initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ThemeService} from './services/theme.service';
+import {NgClass} from '@angular/common';
+import {TranslationService} from '@memoryMatchGame/services';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'memory-match-game';
+  constructor(public themeService: ThemeService,public translation: TranslationService) {}
+
+  toggleDarkMode() {
+    this.themeService.toggleDarkMode();
+  }
 }
